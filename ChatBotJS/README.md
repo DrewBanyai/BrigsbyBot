a0503a0d663a7bcbc1c201be1db899f73b8804a4
Drew Banyai <DrewBanyai@gmail.com>
2021-04-09 15:51:48 -0700

    - Updated the README.md -

--- ChatBotJS/README.md
+++ ChatBotJS/README.md
@@ -1,8 +1,10 @@
 # BrigsbyBot
-A dead-simple skeleton project to start a Javascript Twitch chat bot or response tool
+A dead-simple way to allow chat users in your twitch channel to earn points (through bits or gifts from mods) and
+then spend those points to activate named hot-key combinations on your desktop with the help of the BrigsbyBot 
+"Control Box" program which should be included with this.
 
 GETTING STARTED:
 - Open Settings.js and enter your channel name, the name of your bot, and the oauth for your bot (oauth can be found at https://twitchapps.com/tmi/)
 - Open the index.htm (or Release/BrigsbyBot.htm) file in a browser or in OBS
-- In the given twitch channel's chat, type !help to check if the bot is active
-- Enjoy. Please let me know if you experience any issues with the bot template or need any help.
+- In the given twitch channel's chat, type !brigsby to check if the bot is active
+- Enjoy. Please let me know if you experience any issues with the bot or need any help.
--- ChatBotJS/index.js
+++ ChatBotJS/index.js
@@ -4,6 +4,21 @@ let token = (SETTINGS && SETTINGS.TWITCH_DATA && SETTINGS.TWITCH_DATA.TOKEN) ? S
 let username = (SETTINGS && SETTINGS.TWITCH_DATA && SETTINGS.TWITCH_DATA.USERNAME) ? SETTINGS.TWITCH_DATA.USERNAME : null
 let channel = (SETTINGS && SETTINGS.TWITCH_DATA && SETTINGS.TWITCH_DATA.CHANNEL) ? SETTINGS.TWITCH_DATA.CHANNEL : null;
 
+//  If the token, username, or channel are not provided, attempt to find them in the URL variables
+if (!token || !username || !channel) {
+	let URL_OPTIONS = getOptionsInURL();
+
+	//  If new URL arguments are put in the URL, this will grab those and override the ones set in code (if any)
+	if (URL_OPTIONS && URL_OPTIONS.token) { token = "oauth:" + URL_OPTIONS.token; }
+	if (URL_OPTIONS && URL_OPTIONS.username) { username = URL_OPTIONS.username; }
+	if (URL_OPTIONS && URL_OPTIONS.channel) { channel = URL_OPTIONS.channel; }
+
+	//  If any of the pieces are still not available, display an error
+	if (!token) { console.error("No token variable found in settings or URL variables."); }
+	if (!username) { console.error("No username variable found in settings or URL variables."); }
+	if (!channel) { console.error("No channel variable found in settings or URL variables."); }
+}
+
 let LoadSiteContent = async () => {
     loadSiteMainArea();
 	attemptAutoLogin();
