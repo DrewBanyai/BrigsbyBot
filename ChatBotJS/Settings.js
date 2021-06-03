5533b6c6fa3ccc533d6de0af353d34d1d6886851
Drew Banyai <DrewBanyai@gmail.com>
2021-06-02 19:14:29 -0700

    - Updated to the newest version that includes the channel point rewards responses

--- ChatBotJS/Settings.js
+++ ChatBotJS/Settings.js
@@ -1,13 +1,12 @@
 let SETTINGS = {
     TWITCH_DATA: {
-        CHANNEL: "",
-        USERNAME: "",
-        TOKEN: ""
     },
     MODS_LIST: [
     ],
     MICROSERVICE_URL: "http://localhost:5000/",
     BOT_ITEMS_LIST: {
+    },
+    REWARD_ITEMS_LIST: {
     },
     KEY_TRIGGER_EVENTS: {
     }
